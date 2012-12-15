8c834268c5b33e86049b86a9540ff18bfa8b8854
tomcsmith1990 <tomcsmith1990@localhost>
2012-12-15 13:55:50 +0000

    Added map button - also layout files.

--- SBUS/src/uk/ac/cam/tcs40/sbus/WifiReceiver.java
+++ SBUS/src/uk/ac/cam/tcs40/sbus/WifiReceiver.java
@@ -10,30 +10,31 @@ import android.util.Log;
 
 public class WifiReceiver extends BroadcastReceiver {
 
-	private MapComponent m_mapComponent;
+	private MapComponent m_MapComponent;
 
 	public WifiReceiver(MapComponent mapComponent) {
 		super();
-		this.m_mapComponent = mapComponent;
+		this.m_MapComponent = mapComponent;
 	}
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
 
 		if(intent.getAction().equals(WifiManager.NETWORK_STATE_CHANGED_ACTION)) {
-			
+
 			NetworkInfo networkInfo = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
-			
+
 			if(networkInfo.isConnected()) {
 				// Wifi is connected
 				Log.d("SBUS", "Wifi is connected: " + String.valueOf(networkInfo));
 
-				this.m_mapComponent.map(":44444", "SomeEpt", "192.168.0.6:44444", "SomeEpt");
-							}
+				this.m_MapComponent.map(":44444", "SomeEpt", "192.168.0.6:44444", "SomeEpt");
+			}
+			
 		} else if(intent.getAction().equals(ConnectivityManager.CONNECTIVITY_ACTION)) {
-			
+
 			NetworkInfo networkInfo = intent.getParcelableExtra(ConnectivityManager.EXTRA_NETWORK_INFO);
-			
+
 			if(networkInfo.getType() == ConnectivityManager.TYPE_WIFI && ! networkInfo.isConnected()) {
 				// Wifi is disconnected
 				Log.d("SBUS", "Wifi is disconnected: " + String.valueOf(networkInfo));
