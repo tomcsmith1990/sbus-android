b91b96e8b8afe98d9c197ac771810d7035bc6620
tomcsmith1990 <tomcsmith1990@localhost>
2012-12-15 14:01:30 +0000

    Add some additional comments

--- SBUS/src/uk/ac/cam/tcs40/sbus/Mapper.java
+++ SBUS/src/uk/ac/cam/tcs40/sbus/Mapper.java
@@ -26,25 +26,31 @@ public class Mapper extends Activity
 	{
 		super.onCreate(savedInstanceState);
 
-		// Create the SBUSBootloader.
-		// This will copy all necessary SBUS files.
+		// Copy the needed SBUS files if they do not already exist.
 		new SBUSBootloader(getApplicationContext());
 
+		// .cpt file for this component.
 		final String mapFile = "map.cpt";
-		
+
+		// Copy the .cpt file to the application directory.
 		new FileBootloader(getApplicationContext()).store(mapFile);
 
+		// Initialise and start the map component.
 		this.m_MapComponent = new MapComponent("spoke", "spoke");
 		this.m_MapComponent.addEndpoint("map", "F46B9113DB2D");
 		this.m_MapComponent.start(getApplicationContext().getFilesDir() + "/" + mapFile,  -1, false);
-	
+
+		// Display the layout.
 		setContentView(R.layout.activity_mapper);
-		
+
+		// Add event listener to the map button.
 		Button button = (Button)findViewById(R.id.map_button);
 		button.setOnClickListener(m_MapButtonListener);
 
 		new Thread() {
 			public void run() {
+				// Register a broadcast receiver.
+				// Detects when we connect/disconnect to a Wifi network.
 				IntentFilter intentFilter = new IntentFilter();
 				intentFilter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
 				intentFilter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION);
@@ -60,69 +66,5 @@ public class Mapper extends Activity
 				}
 			}
 		}.start();
-
-		// Create a thread to run the sensor.
-		/*new Thread() {
-			public 		setContentView(R.layout.activity_mapper);void run() {
-			  	// Create a FileBootloader to store our component file.
-  				new FileBootloader(getApplicationContext()).store("SomeSensor.cpt");
-
-				SComponent scomponent = new SComponent("SomeSensor", "sensor-instance");
-				scomponent.addEndpoint("SomeEpt", "BE8A47EBEB58");
-				scomponent.addRDC("192.168.0.3:50123");
-				// 10.0.2.2 is the development machine when running in AVD.
-				//scomponent.addRDC("10.0.2.2:50123");
-				String cptFile = "SomeSensor.cpt";
-				scomponent.start(getApplicationContext().getFilesDir() + "/" + cptFile, -1, true);
-				scomponent.setPermission("SomeConsumer", "", true);
-
-				int i = 0;
-				while (true) {
-
-					scomponent.createMessage("reading");
-					scomponent.packString("This is message #" + i++);
-					scomponent.packInt((int) (Math.random() * 1000), "someval");
-					scomponent.packInt(5, "somevar");
-
-					final String s = scomponent.emit();
-
-					runOnUiThread(new Runnable() {
-						public void run() {
-							tv.setText(s);
-						}
-					});
-
-					try {
-						Thread.sleep(2000);
-					} catch (InterruptedException e) { }
-				}
-			}
-		}.start();*/
-		/*
-		new Thread() {
-			public void run() {				
-				SComponent mapComponent = new SComponent("spoke", "spoke");
-				mapComponent.addEndpoint("map", "F46B9113DB2D");
-				String mapFile = "map.cpt";
-				mapComponent.start(getApplicationContext().getFilesDir() + "/" + mapFile,  -1, false);
-
-				mapComponent.endpointMap(":44444");
-
-				mapComponent.createMessage("map");
-				mapComponent.packString("SomeEpt", "endpoint");
-				mapComponent.packString("192.168.0.3:44444", "peer_address");
-				mapComponent.packString("SomeEpt", "peer_endpoint");
-				mapComponent.packString("", "certificate");
-
-				final String s = mapComponent.emit();
-
-				runOnUiThread(new Runnable() {
-					public void run() {
-						tv.setText(s);
-					}
-				});
-			}
-		}.start();
-		 */	
 	}
 }
