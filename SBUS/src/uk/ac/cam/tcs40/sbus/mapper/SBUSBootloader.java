ce5620719b4aed8545dc6e1c28263116b3432857
tomcsmith1990 <tomcsmith1990@localhost>
2013-01-17 17:12:20 +0000

    and don't try to load them.

--- SBUS/src/uk/ac/cam/tcs40/sbus/mapper/SBUSBootloader.java
+++ SBUS/src/uk/ac/cam/tcs40/sbus/mapper/SBUSBootloader.java
@@ -7,24 +7,9 @@ public class SBUSBootloader extends FileBootloader {
 
 	private final String[] m_Files = 
 		{
-			"idl/broker.cpt",
-			"idl/builtin.cpt",
-			"idl/carpark.cpt",
 			"idl/cpt_metadata.idl",
 			"idl/cpt_status.idl",
-			"idl/demo.cpt",
 			"idl/map_constraints.idl",
-			"idl/rdc_default.priv",
-			"idl/rdc.cpt",
-			"idl/rdcacl.cpt",
-			"idl/sbus.cpt",
-			"idl/slowcar.cpt",
-			"idl/speek.cpt",
-			"idl/spersist.cpt",
-			"idl/spoke.cpt",
-			"idl/trafficgen.cpt",
-			"idl/universalsink.cpt",
-			"idl/universalsource.cpt",
 			"sbuswrapper"
 		};
 
@@ -34,11 +19,11 @@ public class SBUSBootloader extends FileBootloader {
 		// Create and set permissions on /idl directory.
 		mkdir(getApplicationDirectory() + "/idl");
 		setPermissions("idl", 755);
-		
+
 		mkdir(getApplicationDirectory() + "/.sbus/log");
 		setPermissions(".sbus", 777);
 		setPermissions(".sbus/log", 777);
-		
+
 		// Copy the idl files and sbuswrapper if they don't exist.
 		// Also sets correct permissions on them.
 		for (String filename : m_Files) {
