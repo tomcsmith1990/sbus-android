a72bd8b5815c1e188a5b40af99c19e1582133551
tomcsmith1990 <tomcsmith1990@localhost>
2013-01-17 17:46:13 +0000

    some helper methods on getting source component info on a message

--- SBUS/src/uk/ac/cam/tcs40/sbus/SMessage.java
+++ SBUS/src/uk/ac/cam/tcs40/sbus/SMessage.java
@@ -4,6 +4,10 @@ public class SMessage {
 
 	private long m_MessagePtr;
 	
+	private String m_SourceCpt;
+	private String m_SourceInst;
+	private String m_SourceEpt;
+	
 	SMessage(long ptr) {
 		// Note - no modifier means package only access.
 		// Can only create by receiving a message.
@@ -20,6 +24,39 @@ public class SMessage {
 	}
 	
 	/**
+	 * 
+	 * @return The source component of this message.
+	 */
+	public String getSourceComponent() {
+		if (this.m_SourceCpt == null)
+			this.m_SourceCpt = getSourceComponent(this.m_MessagePtr);
+		
+		return this.m_SourceCpt;
+	}
+	
+	/**
+	 * 
+	 * @return The source instance of this message.
+	 */
+	public String getSourceInstance() {
+		if (this.m_SourceInst == null)
+			this.m_SourceInst = getSourceEndpoint(this.m_MessagePtr);
+		
+		return this.m_SourceInst;
+	}
+	
+	/**
+	 * 
+	 * @return The source endpoint of this message.
+	 */
+	public String getSourceEndpoint() {
+		if (this.m_SourceEpt == null)
+			this.m_SourceEpt = getSourceEndpoint(this.m_MessagePtr);
+		
+		return this.m_SourceEpt;
+	}
+	
+	/**
 	 * Delete the native representation of this message.
 	 */
 	public void delete() {
@@ -27,5 +64,8 @@ public class SMessage {
 	}
 	
 	private native long getTree(long ptr);
+	private native String getSourceComponent(long ptr);
+	private native String getSourceInstance(long ptr);
+	private native String getSourceEndpoint(long ptr);
 	private native void delete(long ptr);
 }
