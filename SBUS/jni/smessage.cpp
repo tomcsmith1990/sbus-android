1506836f41e2f1714d1a3a47e0ba3fd2beeb252d
tomcsmith1990 <tomcsmith1990@localhost>
2013-01-17 17:46:13 +0000

    some helper methods on getting source component info on a message

--- SBUS/jni/smessage.cpp
+++ SBUS/jni/smessage.cpp
@@ -20,4 +20,34 @@ Java_uk_ac_cam_tcs40_sbus_SMessage_getTree( JNIEnv* env,
 	return (long)sn;
 }
 
+jstring
+Java_uk_ac_cam_tcs40_sbus_SMessage_getSourceComponent( JNIEnv* env,
+						                              	jobject thiz,
+						                              	jlong message)
+{
+	const char *source_cpt;
+	source_cpt = ((smessage *)message)->source_cpt;
+	return env->NewStringUTF(source_cpt);
+}
+
+jstring
+Java_uk_ac_cam_tcs40_sbus_SMessage_getSourceInstance( JNIEnv* env,
+						                              	jobject thiz,
+						                              	jlong message)
+{
+	const char *source_inst;
+	source_inst = ((smessage *)message)->source_inst;
+	return env->NewStringUTF(source_inst);
+}
+
+jstring
+Java_uk_ac_cam_tcs40_sbus_SMessage_getSourceEndpoint( JNIEnv* env,
+						                              	jobject thiz,
+						                              	jlong message)
+{
+	const char *source_ep;
+	source_ep = ((smessage *)message)->source_ep;
+	return env->NewStringUTF(source_ep);
+}
+
 }
