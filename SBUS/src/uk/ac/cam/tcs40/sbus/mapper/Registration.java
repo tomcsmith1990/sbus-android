4cbdd1a9493c1db6c001a97f038788a80efbf53f
tomcsmith1990 <tomcsmith1990@localhost>
2013-02-09 17:01:42 +0000

    Allow registrations to add MapPolicy, and then apply all policies for each registration

--- SBUS/src/uk/ac/cam/tcs40/sbus/mapper/Registration.java
+++ SBUS/src/uk/ac/cam/tcs40/sbus/mapper/Registration.java
@@ -10,6 +10,7 @@ public class Registration {
 	private final String m_Port, m_Component, m_Instance;
 	private Date m_Date;
 	private final List<Permission> m_Permissions;
+	private final List<MapPolicy> m_MapPolicies;
 
 	public Registration(String port, String component, String instance) {
 		this.m_Port = port;
@@ -17,6 +18,7 @@ public class Registration {
 		this.m_Instance = instance;
 		update();
 		this.m_Permissions = new LinkedList<Permission>();
+		this.m_MapPolicies = new LinkedList<MapPolicy>();
 	}
 	
 	public void addPermission(String component, String instance, boolean allow) {
@@ -34,6 +36,14 @@ public class Registration {
 		
 		if (!exists)
 			this.m_Permissions.add(new Permission(component, instance, allow));
+	}
+	
+	public void addMapPolicy(String localEndpoint, String peerComponent, String peerEndpoint) {
+		this.m_MapPolicies.add(new MapPolicy(localEndpoint, peerComponent, peerEndpoint));
+	}
+	
+	public List<MapPolicy> getMapPolicies() {
+		return this.m_MapPolicies;
 	}
 	
 	public String getPort() {
