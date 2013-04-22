053aa3dce06dc3a8cb50a390c5a8b40306019232
tomcsmith1990 <tomcsmith1990@localhost>
2013-04-22 16:14:56 +0000

    Diss: JNI graph

---
+++ SBUS-Android/diss/evaluation/jni/jni.m
@@ -0,0 +1,16 @@
+delimiter = '\n';
+
+range = [1 0 100 0];
+
+java = dlmread('jni-java.txt', delimiter, range);
+cpp = dlmread('jni.txt', delimiter, range);
+
+diff = java - cpp;
+
+barh([0 1], [mean(cpp)/1000 mean(diff)/1000 ; 0 0 ], 0.2, 'stacked');
+ylim([-.5 .5]);
+set(gca, 'YTick', []);
+xlabel('Breakdown of time taken to perform a JNI call (\mus)');
+legend({'C++', 'JNI'});
+
+print -depsc 'jni.eps';
