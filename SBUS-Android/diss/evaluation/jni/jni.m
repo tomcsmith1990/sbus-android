462a488e0a377daf4e410c43008e058ee9ff9649
tomcsmith1990 <tomcsmith1990@localhost>
2013-04-24 11:01:33 +0000

    Diss: eval 4.2 explain why test is important

--- SBUS-Android/diss/evaluation/jni/jni.m
+++ SBUS-Android/diss/evaluation/jni/jni.m
@@ -11,6 +11,6 @@ barh([0 1], [mean(cpp)/1000 mean(diff)/1000 ; 0 0 ], 0.2, 'stacked');
 ylim([-.5 .5]);
 set(gca, 'YTick', []);
 xlabel('Breakdown of time taken to perform a JNI call (\mus)');
-legend({'C++', 'JNI'});
+legend({'C++', 'Java'});
 
 print -depsc 'jni.eps';
