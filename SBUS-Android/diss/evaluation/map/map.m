0b2a38baa2df22786be27771de9fa3c287a8dc75
tomcsmith1990 <tomcsmith1990@localhost>
2013-05-12 15:49:17 +0000

    Diss: change map test

--- SBUS-Android/diss/evaluation/map/map.m
+++ SBUS-Android/diss/evaluation/map/map.m
@@ -1,49 +1,40 @@
 delimiter = '\n';
 
-range1 = [0 0 99 0];
-range2 = [100 0 199 0];
+range = [0 0 99 0];
 
-laptop_laptop = dlmread('spoke-map.txt', delimiter, range1) / 1000;
-laptop_phone = dlmread('spoke-map.txt', delimiter, range2) / 1000;
-phone_laptop = dlmread('spoke-map-phone.txt', delimiter, range1) / 1000;
-phone_phone = dlmread('spoke-map-phone.txt', delimiter, range2) / 1000;
+laptop = dlmread('spoke-map-laptop.txt', delimiter, range) / 1000;
+phone = dlmread('spoke-map-phone.txt', delimiter, range) / 1000;
+remote = dlmread('spoke-map-remote.txt', delimiter, range) / 1000;
 
-disp('laptop_laptop mean = ')
-disp(mean(laptop_laptop))
+disp('laptop mean = ')
+disp(mean(laptop))
 
-disp('laptop_laptop std = ')
-disp(std(laptop_laptop))
+disp('laptop std = ')
+disp(std(laptop))
 
-disp('laptop_phone mean = ')
-disp(mean(laptop_phone))
+disp('phone mean = ')
+disp(mean(phone))
 
-disp('laptop_phone std = ')
-disp(std(laptop_phone))
+disp('phone std = ')
+disp(std(phone))
 
-disp('phone_laptop mean = ')
-disp(mean(phone_laptop))
+disp('remote mean = ')
+disp(mean(remote))
 
-disp('phone_laptop std = ')
-disp(std(phone_laptop))
-
-disp('phone_phone mean = ')
-disp(mean(phone_phone))
-
-disp('phone_phone std = ')
-disp(std(phone_phone))
+disp('remote std = ')
+disp(std(remote))
 
 hold all;
 
-errorbar(1, mean(laptop_laptop), std(laptop_laptop), '-o');
-errorbar(2, mean(laptop_phone), std(laptop_phone), '-+');
-errorbar(3, mean(phone_laptop), std(phone_laptop), '-*');
-errorbar(4, mean(phone_phone), std(phone_phone), '-X');
+errorbar(1, mean(remote), std(remote), '-o');
+errorbar(2, mean(phone), std(phone), '-+');
+errorbar(3, mean(laptop), std(laptop), '-*');
 
-xlabel('Consumer-Producer', 'FontSize', 12);
+xlabel('Producer', 'FontSize', 12);
 ylabel('Time (ms)', 'FontSize', 12);
-title('Connection Times Between Producers and Consumers on a Laptop and Phone', 'FontSize', 12);
-set(gca, 'XTick', [1 2 3 4]);
-set(gca, 'XTickLabel', {'Laptop-Laptop', 'Laptop-Phone', 'Phone-Laptop', 'Phone-Phone'});
+title('Connection Times Between Producers on Different Devices', 'FontSize', 12);
+set(gca, 'XTick', [1 2 3]);
+set(gca, 'XTickLabel', {'Local Machine', 'Remote Machine', 'Remote Phone'});
 
 print -depsc 'map.eps';
 
