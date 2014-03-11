d59671d3c28fa594296ed66658e9ddb58eaa9831
tomcsmith1990 <tomcsmith1990@localhost>
2014-03-11 20:47:58 +0000

    Method to create the policy sink endpoint

--- SBUS/src/uk/ac/cam/tcs40/sbus/mapper/PolicyDirectory.java
+++ SBUS/src/uk/ac/cam/tcs40/sbus/mapper/PolicyDirectory.java
@@ -1,13 +1,21 @@
 package uk.ac.cam.tcs40.sbus.mapper;
 
 import uk.ac.cam.tcs40.sbus.SComponent;
+import uk.ac.cam.tcs40.sbus.SEndpoint;
+import uk.ac.cam.tcs40.sbus.SComponent.EndpointType;
 
 class PolicyDirectory {
 
 	private SComponent m_Component;
+	private SEndpoint m_PolicySink;
 
 	PolicyDirectory(SComponent component) {
 		m_Component = component;
+	}
+
+	SEndpoint addPolicySinkEndpoint() {
+		m_PolicySink = m_Component.addEndpoint("map_policy", EndpointType.EndpointSink, "157EC474FA55");
+		return m_PolicySink;
 	}
 
 }
