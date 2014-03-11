828aab2b9cddfba3e0f6f9883ecf6580b87b45c5
tomcsmith1990 <tomcsmith1990@localhost>
2014-03-11 20:38:39 +0000

    Make AirsEndpointManager package visibilty, and don't return endpoint when not needed

--- SBUS/src/uk/ac/cam/tcs40/sbus/mapper/AirsEndpointManager.java
+++ SBUS/src/uk/ac/cam/tcs40/sbus/mapper/AirsEndpointManager.java
@@ -8,38 +8,37 @@ import uk.ac.cam.tcs40.sbus.SNode;
 import uk.ac.cam.tcs40.sbus.SComponent.EndpointType;
 import uk.ac.cam.tcs40.sbus.SEndpoint;
 
-public class AirsEndpointManager {
+class AirsEndpointManager {
 
 	private SComponent m_Component;
 	private SEndpoint m_DataSinkEndpoint;
 	private SEndpoint m_SubscriptionEndpoint;
 	private final List<String> m_AirsSubscriptions = new LinkedList<String>();
 
-	public AirsEndpointManager(SComponent component) {
+	AirsEndpointManager(SComponent component) {
 		m_Component = component;
 	}
 
-	public SEndpoint addDataSinkEndpoint() {
+	SEndpoint addDataSinkEndpoint() {
 		m_DataSinkEndpoint = m_Component.addEndpoint("AIRS", EndpointType.EndpointSink, "6187707D4CCE");
 		return m_DataSinkEndpoint;
 	}
 
-	public SEndpoint addSubscriptionEndpoint() {
+	void addSubscriptionEndpoint() {
 		m_SubscriptionEndpoint = m_Component.addEndpoint("airs_subscribe", EndpointType.EndpointSource, "F03F918E91A3");
-		return m_SubscriptionEndpoint;
 	}
 	
-	public void unmapDataSinkEndpoint() {
+	void unmapDataSinkEndpoint() {
 		m_DataSinkEndpoint.unmap();
 		m_DataSinkEndpoint = null;
 	}
 	
-	public void unmapSubscriptionEndpoint() {
+	void unmapSubscriptionEndpoint() {
 		m_SubscriptionEndpoint.unmap();
 		m_SubscriptionEndpoint.unmap();
 	}
 	
-	public void subscribeToAIRS(String airsAddress, String sensorCode) {
+	void subscribeToAIRS(String airsAddress, String sensorCode) {
 		if (airsAddress == null || sensorCode == null) 
 			return;
 
