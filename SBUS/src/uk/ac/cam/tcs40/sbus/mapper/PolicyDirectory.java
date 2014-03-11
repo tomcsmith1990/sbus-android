86adf55efe76943d7511e1eb9acbff93830f5f3b
tomcsmith1990 <tomcsmith1990@localhost>
2014-03-11 20:46:24 +0000

    Assign PolicyDirectory just before we create relevant endpoint

--- SBUS/src/uk/ac/cam/tcs40/sbus/mapper/PolicyDirectory.java
+++ SBUS/src/uk/ac/cam/tcs40/sbus/mapper/PolicyDirectory.java
@@ -1,5 +1,13 @@
 package uk.ac.cam.tcs40.sbus.mapper;
 
+import uk.ac.cam.tcs40.sbus.SComponent;
+
 class PolicyDirectory {
+
+	private SComponent m_Component;
+
+	PolicyDirectory(SComponent component) {
+		m_Component = component;
+	}
 
 }
