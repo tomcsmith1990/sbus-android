ad263ea4f036544a72e3d7a336cb6af42b4e78cd
tomcsmith1990 <tomcsmith1990@localhost>
2014-03-11 21:26:16 +0000

    Change the permissions within the class

--- SBUS/src/uk/ac/cam/tcs40/sbus/mapper/ComponentPermissions.java
+++ SBUS/src/uk/ac/cam/tcs40/sbus/mapper/ComponentPermissions.java
@@ -2,6 +2,8 @@ package uk.ac.cam.tcs40.sbus.mapper;
 
 import uk.ac.cam.tcs40.sbus.SComponent;
 import uk.ac.cam.tcs40.sbus.SEndpoint;
+import uk.ac.cam.tcs40.sbus.SMessage;
+import uk.ac.cam.tcs40.sbus.SNode;
 import uk.ac.cam.tcs40.sbus.SComponent.EndpointType;
 
 class ComponentPermissions {
@@ -21,6 +23,46 @@ class ComponentPermissions {
 	void unmapPermissionsEndpoint() {
 		m_PermissionsEndpoint.unmap();
 		m_PermissionsEndpoint = null;
+	}
+	
+	void changePermissions() {
+		String targetComponent, targetInstance, //targetAddress, targetEndpoint,
+		remoteComponent, remoteInstance, 
+		sourceComponent, sourceInstance;
+		boolean allow;
+
+		SMessage message;
+		SNode snode;
+
+		message = m_PermissionsEndpoint.receive();
+		sourceComponent = message.getSourceComponent();
+		sourceInstance = message.getSourceInstance();
+
+		snode = message.getTree();
+
+		targetComponent = snode.extractString("target_cpt");
+		targetInstance = snode.extractString("target_inst");
+		//targetAddress = snode.extractString("target_address");
+		//targetEndpoint = snode.extractString("target_endpt");
+		remoteComponent = snode.extractString("principal_cpt");
+		remoteInstance = snode.extractString("principal_inst");
+		allow = snode.extractBoolean("add_perm");
+
+		message.delete();
+
+		// Check that the component is setting a permission for itself.
+		if (!targetComponent.equals(sourceComponent) || !targetInstance.equals(sourceInstance))
+			return;
+
+		if (targetComponent.equals("rdc")) {
+			// local rule, handled by wrapper.
+			m_Component.setPermission(remoteComponent, remoteInstance, allow);
+		} else {
+			Registration registration = RegistrationRepository.find(targetComponent, targetInstance);
+			if (registration != null) {
+				registration.addPermission(remoteComponent, remoteInstance, allow);
+			}
+		}
 	}
 
 }
