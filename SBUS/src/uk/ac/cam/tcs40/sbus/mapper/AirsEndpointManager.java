192b6465063d5041ca50379e26770675ba1d2a39
tomcsmith1990 <tomcsmith1990@localhost>
2014-03-11 20:36:03 +0000

    Move the subscribeToAirs method to the new class

--- SBUS/src/uk/ac/cam/tcs40/sbus/mapper/AirsEndpointManager.java
+++ SBUS/src/uk/ac/cam/tcs40/sbus/mapper/AirsEndpointManager.java
@@ -1,6 +1,10 @@
 package uk.ac.cam.tcs40.sbus.mapper;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import uk.ac.cam.tcs40.sbus.SComponent;
+import uk.ac.cam.tcs40.sbus.SNode;
 import uk.ac.cam.tcs40.sbus.SComponent.EndpointType;
 import uk.ac.cam.tcs40.sbus.SEndpoint;
 
@@ -9,6 +13,7 @@ public class AirsEndpointManager {
 	private SComponent m_Component;
 	private SEndpoint m_DataSinkEndpoint;
 	private SEndpoint m_SubscriptionEndpoint;
+	private final List<String> m_AirsSubscriptions = new LinkedList<String>();
 
 	public AirsEndpointManager(SComponent component) {
 		m_Component = component;
@@ -32,6 +37,21 @@ public class AirsEndpointManager {
 	public void unmapSubscriptionEndpoint() {
 		m_SubscriptionEndpoint.unmap();
 		m_SubscriptionEndpoint.unmap();
+	}
+	
+	public void subscribeToAIRS(String airsAddress, String sensorCode) {
+		if (airsAddress == null || sensorCode == null) 
+			return;
+
+		if (!m_AirsSubscriptions.contains(sensorCode)) {
+			m_SubscriptionEndpoint.map(airsAddress, "subscribe");
+			SNode subscription = m_SubscriptionEndpoint.createMessage("subscription");
+			subscription.packString(sensorCode, "sensor");
+			m_SubscriptionEndpoint.emit(subscription);
+			m_SubscriptionEndpoint.unmap();
+
+			m_AirsSubscriptions.add(sensorCode);
+		}
 	}
 
 }
