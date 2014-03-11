fe5a683dd8b0b96e81a4abe62775219394f3f6b1
tomcsmith1990 <tomcsmith1990@localhost>
2014-03-11 21:19:49 +0000

    Accept registrations in the new class. Also holds the AIRS server address.

--- SBUS/src/uk/ac/cam/tcs40/sbus/mapper/ComponentRegister.java
+++ SBUS/src/uk/ac/cam/tcs40/sbus/mapper/ComponentRegister.java
@@ -2,12 +2,15 @@ package uk.ac.cam.tcs40.sbus.mapper;
 
 import uk.ac.cam.tcs40.sbus.SComponent;
 import uk.ac.cam.tcs40.sbus.SEndpoint;
+import uk.ac.cam.tcs40.sbus.SMessage;
+import uk.ac.cam.tcs40.sbus.SNode;
 import uk.ac.cam.tcs40.sbus.SComponent.EndpointType;
 
 class ComponentRegister {
 
 	private SComponent m_Component;
 	private SEndpoint m_RegistrationEndpoint;
+	private String m_AirsServerAddress;
 
 	ComponentRegister(SComponent component) {
 		m_Component = component;
@@ -21,6 +24,55 @@ class ComponentRegister {
 	void unmapRegistrationEndpoint() {
 		m_RegistrationEndpoint.unmap();
 		m_RegistrationEndpoint = null;		
+	}
+	
+	String getAirsServerAddress() { 
+		return m_AirsServerAddress;
+	}
+	
+	void acceptRegistration(String phoneIp) {
+		String address, host, port, sourceComponent, sourceInstance;
+		boolean register;
+		SMessage message;
+		SNode snode;
+
+		message = m_RegistrationEndpoint.receive();
+		sourceComponent = message.getSourceComponent();
+		sourceInstance = message.getSourceInstance();
+
+		snode = message.getTree();
+		address = snode.extractString("address");
+		register = snode.extractBoolean("arrived");
+
+		message.delete();
+
+		host = address.split(":")[0];
+		port = address.split(":")[1];
+
+		// Check this is a component on the phone.
+		if (!host.equals(phoneIp) && !host.equals("127.0.0.1") && !host.equals("") && !host.equals("10.0.2.15"))
+			return;
+
+		if (register) {
+
+			if (sourceComponent.equals("AirsSensor")) {
+				m_AirsServerAddress = ":" + port;
+				PMCActivity.addStatus("Found AIRS at :" + port);
+				return;
+				//subscribeToAIRS("WC");
+			}
+
+			Registration registration = RegistrationRepository.add(port, sourceComponent, sourceInstance);
+			if (registration != null) {
+				PMCActivity.addStatus("Registered component " + sourceComponent + " instance " + sourceInstance + ", at :" + port);
+			} else {
+				PMCActivity.addStatus("Attempting to register already registered component " + sourceComponent + ":" + sourceInstance);
+			}
+
+		} else {
+			Registration deregistration = RegistrationRepository.remove(port);
+			PMCActivity.addStatus("Deregistered component " + deregistration.getComponentName() + ":" + deregistration.getInstanceName() + " at :" + port);
+		}
 	}
 
 }
